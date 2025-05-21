bdab7c2e844c5c9e29f8ee780df3cc50b7b69d4e
Prassanna <nivesh.prassanna@studentfr.ch>
2025-05-21 14:06:36 +0200

    test ex4

--- exercices/exercice04/EchangeValeurs.java
+++ exercices/exercice04/EchangeValeurs.java
@@ -1,6 +1,6 @@
 package exercices.exercice04;
 
-public class echangeValeurs {
+public class EchangeValeurs {
     public static void main(String[] args) {
         int variable1 = 1;
         int variable2 = 2;
