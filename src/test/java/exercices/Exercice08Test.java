867ff9231c7fad4dab1df71d8a2d23e6a081f647
Prassanna <nivesh.prassanna@studentfr.ch>
2025-05-22 15:21:35 +0200

    ex 8 test

--- src/test/java/exercices/Exercice08Test.java
+++ src/test/java/exercices/Exercice08Test.java
@@ -8,9 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Exercice08Test {
     @Test
     public void testDossiersBoucles() {
-        assertTrue(new File("exercices/exercice08/ExerciceBoucle1/src/App.java").exists(),
+        assertTrue(new File("exercices/exercice08/ExerciceBoucles1/src/App.java").exists(),
             "❌ App.java n'a pas été trouvé dans le chemin exercice08/ExerciceBoucle1/src/");
-        assertTrue(new File("exercices/exercice08/ExerciceBoucle2/src/App.java").exists(),
+        assertTrue(new File("exercices/exercice08/ExerciceBoucles2/src/App.java").exists(),
             "❌ App.java n'a pas été trouvé dans le chemin exercice08/ExerciceBoucle2/src/");
     }
 }
