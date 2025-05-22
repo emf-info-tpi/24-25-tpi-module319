ebd4e803a85c8fe94ddee0842e7a6db99c60bd96
Prassanna <nivesh.prassanna@studentfr.ch>
2025-05-22 15:18:04 +0200

    test ex 11 2

--- src/test/java/exercices/Exercice11Test.java
+++ src/test/java/exercices/Exercice11Test.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Exercice11Test {
     @Test
     public void testFichierInversion() {
-        assertTrue(new File("24-25-tpi-module319/exercices/exercice11/ExercicesInversionTableau/src/App.java").exists(),
-            "❌ App.java n'a pas été trouvé dans le dossier ExercicesInversionTableaux");
+        assertTrue(new File("exercices/exercice11/ExercicesInversionTableau/src/App.java").exists(), "❌ App.java n'a pas été trouvé dans le dossier ExercicesInversionTableaux");
     }
 }
