705fb0a7d714e8d50d58ec76cfa6b0cbfe24d42b
Prassanna <nivesh.prassanna@studentfr.ch>
2025-05-23 16:12:57 +0200

    Exercice 5 rechanger

--- exercices/exercice05/ExerciceCondition1.java
+++ exercices/exercice05/ExerciceCondition1.java
@@ -5,10 +5,10 @@ public class ExerciceCondition1 {
     public static void main(String[] args) {
         int valeurATester = -7;
 
-        if (valeurATester > 0)
+        if (valeurATester > 0){
             System.out.println("La valeur est positive");
-        else
+        }else{
             System.out.println("La valeur est négative");
-        
+        }
     }
 }
