e8ed368ac1a930de926e18abdd0a8b66620b700a
Prassanna <nivesh.prassanna@studentfr.ch>
2025-05-30 16:22:52 +0200

    exercice 2 checkstyle validé ?

--- exercices/exercice02/Exercice02WithErrors.java
+++ exercices/exercice02/Exercice02WithErrors.java
@@ -1,6 +1,6 @@
 package exercices.exercice02;
 
-public class Exercice02WithErrors {
+public class exercice02WithErrors {
     public static void main(String[] args) {
     System.out.println("Hello how are you?"); // Mauvais message 
     System.out.println("Bizzare"); // Ligne correcte
